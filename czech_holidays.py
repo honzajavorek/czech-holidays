38dc50c2c94508af2966353a0793fceecba71d71
Honza Javorek <mail@honzajavorek.cz>
2015-08-29 12:50:52 +0200

    Version bump.

--- czech_holidays.py
+++ czech_holidays.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 
 
 __title__ = 'czech-holidays'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __author__ = 'Honza Javorek'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2013 Honza Javorek'
